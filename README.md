c64dd23a22aab528b95f05147f96074ae33f8713
kwicautocaptionbot23 <116583792+kwicautocaptionbot23@users.noreply.github.com>
2022-10-24 22:07:14 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+## <b>𝙈𝙏 𝘼𝙪𝙩𝙤𝘾𝙖𝙥𝙩𝙞𝙤𝙣 𝘽𝙤𝙩</b>
+
+This Repo is Telegram Channel Code to help build a bot that automatically adds captions to files added to a Telegram channel
+
+
+  </a>
+</p>
+<p align="center">
+  <a href="https://github.com/PR0FESS0R-99/AutoCaptionBot-V1/stargazers">
+    <img src="https://img.shields.io/github/stars/PR0FESS0R-99/AutoCaptionBot-V1?style=social">
+
+  </a>
+  
+  <a href="https://github.com/PR0FESS0R-99/AutoCaptionBot-V1/fork">
+    <img src="https://img.shields.io/github/forks/PR0FESS0R-99/AutoCaptionBot-V1?label=Fork&style=social">
+
+  </a>  
+</p>
+
+
+### Deploy To Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/PR0FESS0R-99/AutoCaptionBot-V1)                     
+
+### Simple Variables 😉
+
+* `app_id` : Get this value from [telegram.org](https://my.telegram.org/apps) or [@MT_MyTelegramOrg_Bot](https://t.me/MT_MyTelegramOrg_Bot) .
+* `api_hash` : Get this value from [telegram.org](https://my.telegram.org/apps) or [@MT_MyTelegramOrg_Bot](https://t.me/MT_MyTelegramOrg_Bot) .
+* `bot_token` : Create a bot using [@BotFather](https://telegram.dog/BotFather), and get the Telegram API token .
+* `custom_caption` : your caption. add {file_name} to get file original name. More info to watch [YT TUTORIAL](https://github.com/PR0FESS0R-99) .
+
+### Made With 
+
+● 📚 𝗟𝗔𝗡𝗚𝗨𝗔𝗚𝗘
+
+[![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)](https://www.python.org)
+
+● 🧮 𝗟𝗜𝗕𝗥𝗔𝗥𝗬
+
+[![PYROGRAM](https://img.shields.io/badge/%F0%9F%94%A5-PYROGRAM%20-orange)](https://docs.pyrogram.org)
+
+
+### Developer & Support
+
+[Telegram Channel](https://Telegram.dog/Mo_Tech_YT)
+
+[Follow On InstaGram](https://www.instagram.com/mrk_yt_)
+
+[Subscribe YouTube Channel](https://youtube.com/c/MoTech_YT)
+
+### Credits
+
+
+### LICENSE
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/PR0FESS0R-99/AutoCaptionBot-V1/blob/main/LICENSE)
+
+Licensed under [MIT License](https://github.com/PR0FESS0R-99/AutoCaptionBot-V1/blob/main/LICENSE)
+🚫 Don't Sell This Code. Code is fully open source project ❤️‍🔥
+Copyright Claimed by © @PR0FESS0R-99
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,92 @@
+import pyrogram, os, asyncio
+
+try: app_id = int(os.environ.get("app_id", None))
+except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
+try: api_hash = os.environ.get("api_hash", None)
+except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
+try: bot_token = os.environ.get("bot_token", None)
+except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
+try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
+except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
+
+AutoCaptionBotV1 = pyrogram.Client(
+   name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
+
+start_message = """
+<b>👋Hello {}</b>
+<b>I am an AutoCaption bot</b>
+<b>All you have to do is add me to your channel and I will show you my power</b>
+<b>@kwicbotupdates</b>"""
+
+about_message = """
+<b>• Name : [AutoCaption V1](t.me/{username})</b>
+<b>• Developer : [KWIC](https://t.me/kwicbotupdates)
+<b>• Language : Python3</b>
+<b>• Library : Pyrogram v{version}</b>
+<b>• Updates : <a href=https://t.me/kwicbotupdates>Click Here</a></b>
+<b>• Source Code : <a href=https://github.com/PR0-99/CaptionBot-V1>Click Here</a></b>"""
+
+@AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
+def start_command(bot, update):
+  update.reply(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("start"))
+def strat_callback(bot, update):
+  update.message.edit(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+
+@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
+def about_callback(bot, update): 
+  bot = bot.get_me()
+  update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+
+@AutoCaptionBotV1.on_message(pyrogram.filters.channel)
+def edit_caption(bot, update: pyrogram.types.Message):
+  motech, _ = get_file_details(update)
+  try:
+      try: update.edit(custom_caption.format(file_name=motech.file_name))
+      except pyrogram.errors.FloodWait as FloodWait:
+          asyncio.sleep(FloodWait.value)
+          update.edit(custom_caption.format(file_name=motech.file_name))
+  except pyrogram.errors.MessageNotModified: pass 
+    
+def get_file_details(update: pyrogram.types.Message):
+  if update.media:
+    for message_type in (
+        "photo",
+        "animation",
+        "audio",
+        "document",
+        "video",
+        "video_note",
+        "voice",
+        # "contact",
+        # "dice",
+        # "poll",
+        # "location",
+        # "venue",
+        "sticker"
+    ):
+        obj = getattr(update, message_type)
+        if obj:
+            return obj, obj.file_id
+
+def start_buttons(bot, update):
+  bot = bot.get_me()
+  buttons = [[
+   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/kwicbotupdates"),
+   pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
+   ],[
+   pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
+   ]]
+  return pyrogram.types.InlineKeyboardMarkup(buttons)
+
+def about_buttons(bot, update):
+  buttons = [[
+   pyrogram.types.InlineKeyboardButton("🏠 Back To Home 🏠", callback_data="start")
+   ]]
+  return pyrogram.types.InlineKeyboardMarkup(buttons)
+
+print("Telegram AutoCaption V1 Bot Start")
+print("Bot Created By https://t.me/kwicbotupdates")
+
+AutoCaptionBotV1.run()
