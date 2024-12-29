ac75e7e71207b32ac94453b1916b1d017c6e47e9
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-29 17:58:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,17 @@
 
 import pyrogram, os, asyncio
 
-try: app_id = int(os.environ.get("app_id", "20389440"))
-except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
-try: api_hash = os.environ.get("api_hash", "a1a06a18eb9153e9dbd447cfd5da2457")
-except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
-try: bot_token = os.environ.get("bot_token", "6564513574:AAH3Y97iqQjSlV5vKKZdGDUohlhpA-LeSbw")
-except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
-try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
-except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
+app_id = int(os.environ.get("app_id", ""))
+api_hash = os.environ.get("api_hash", "")
+bot_token = os.environ.get("bot_token", "")
+custom_caption = os.environ.get("custom_caption", "`{file_name}`")
 
-AutoCaptionBotV1 = pyrogram.Client(
-   name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
+AutoCaptionBotV1 = pyrogram.Client(name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
 
 start_message = """
 <b>👋Hello {}</b>
 <b>I am an AutoCaption bot</b>
-<b>All you have to do is add me to your channel and I will show you my power</b>
+<b>All you have to do is to add me to your channel as admin and I will show you my power</b>
 <b>@VJ_Botz</b>"""
 
 about_message = """
@@ -32,66 +27,65 @@ about_message = """
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
-  update.reply(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+    update.reply(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
 @AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("start"))
 def strat_callback(bot, update):
-  update.message.edit(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+    update.message.edit(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
 @AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
 def about_callback(bot, update): 
-  bot = bot.get_me()
-  update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+    bot = bot.get_me()
+    update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.channel)
 def edit_caption(bot, update: pyrogram.types.Message):
-  motech, _ = get_file_details(update)
-  try:
-      try: update.edit(custom_caption.format(file_name=motech.file_name))
-      except pyrogram.errors.FloodWait as FloodWait:
-          asyncio.sleep(FloodWait.value)
-          update.edit(custom_caption.format(file_name=motech.file_name))
-  except pyrogram.errors.MessageNotModified: pass 
+    techvj, _ = get_file_details(update)
+    try:
+       try:
+           update.edit(custom_caption.format(file_name=techvj.file_name))
+       except pyrogram.errors.FloodWait as FloodWait:
+           asyncio.sleep(FloodWait.value)
+           update.edit(custom_caption.format(file_name=techvj.file_name))
+       except:
+          pass
+    except pyrogram.errors.MessageNotModified:
+        pass 
     
 def get_file_details(update: pyrogram.types.Message):
-  if update.media:
-    for message_type in (
-        "photo",
-        "animation",
-        "audio",
-        "document",
-        "video",
-        "video_note",
-        "voice",
-        # "contact",
-        # "dice",
-        # "poll",
-        # "location",
-        # "venue",
-        "sticker"
-    ):
-        obj = getattr(update, message_type)
-        if obj:
-            return obj, obj.file_id
+    if update.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            "sticker"
+        ):
+            obj = getattr(update, message_type)
+            if obj:
+                return obj, obj.file_id
 
 def start_buttons(bot, update):
-  bot = bot.get_me()
-  buttons = [[
-   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/VJ_Botz"),
-   pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
-   ],[
-   pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
-   ]]
-  return pyrogram.types.InlineKeyboardMarkup(buttons)
+    bot = bot.get_me()
+    buttons = [[
+        pyrogram.types.InlineKeyboardButton("Updates", url="t.me/VJ_Botz"),
+        pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
+    ],[
+        pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
+    ]]
+    return pyrogram.types.InlineKeyboardMarkup(buttons)
 
 def about_buttons(bot, update):
-  buttons = [[
-   pyrogram.types.InlineKeyboardButton("🏠 Back To Home 🏠", callback_data="start")
-   ]]
-  return pyrogram.types.InlineKeyboardMarkup(buttons)
+    buttons = [[
+        pyrogram.types.InlineKeyboardButton("🏠 Back To Home 🏠", callback_data="start")
+    ]]
+    return pyrogram.types.InlineKeyboardMarkup(buttons)
 
 print("Telegram AutoCaption V1 Bot Start")
-print("Bot Created By https://t.me/VJ_Botz")
+print("Bot Created By @VJ_Botz")
 
 AutoCaptionBotV1.run()
 
